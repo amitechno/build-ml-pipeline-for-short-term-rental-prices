2c03b71f6cf2078baf6392b1c58cc65d6a5bc5b1
Amit Sharma <amit.sharma8@natwest.com>
2023-07-10 00:05:53 +0530

    dropping outliers

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -27,8 +27,14 @@ def go(args):
     df = pd.read_csv(artifact_local_path )
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
+
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
+    
+    idv = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idv].copy()
+
+
     df.to_csv("clean_sample.csv", index=False)
     
     artifact = wandb.Artifact(
